a0572102bb081618387eddbd23a31a6e476e2102
Vince Alshie Rivera <202280400@psu.palawan.edu.ph>
2026-02-26 04:26:15 +0800

    fix static files settings for PythonAnywhere deployment

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -117,8 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = (
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
     BASE_DIR / 'static',
-)
+]
