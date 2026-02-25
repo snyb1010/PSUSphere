4238d4205ee6297d6dcdf535e7e938a6b988a0e9
Vince Alshie Rivera <202280400@psu.palawan.edu.ph>
2026-02-26 02:54:32 +0800

    refactor admin.py

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -1,8 +1,37 @@
 from django.contrib import admin
 from .models import College, Program, Organization, Student, OrgMember
 
-admin.site.register(College)
-admin.site.register(Program)
-admin.site.register(Organization)
-admin.site.register(Student)
-admin.site.register(OrgMember)
+# ----------------------------
+# Task A – CollegeAdmin
+# ----------------------------
+@admin.register(College)
+class CollegeAdmin(admin.ModelAdmin):
+    list_display = ("college_name", "created_at", "updated_at")
+    search_fields = ("college_name",)
+    list_filter = ("created_at",)
+
+# ----------------------------
+# Task B – ProgramAdmin
+# ----------------------------
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ("prog_name", "college")
+    search_fields = ("prog_name", "college__college_name")
+    list_filter = ("college",)
+
+# ----------------------------
+# Task C – OrganizationAdmin
+# ----------------------------
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ("name", "college", "description")
+    search_fields = ("name", "description")
+    list_filter = ("college",)
+
+# ----------------------------
+# StudentAdmin
+# ----------------------------
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("student_id", "lastname", "firstname", "middlename", "program")
+    search_fields = ("lastname", "firstname")
