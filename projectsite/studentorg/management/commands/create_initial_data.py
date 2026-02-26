0fb28e2a54f1235ae0b6d730188f01d99e210481
Vince Alshie Rivera <202280400@psu.palawan.edu.ph>
2026-02-26 09:10:17 +0800

    Add College and Program generation to create_initial_data command

--- projectsite/studentorg/management/commands/create_initial_data.py
+++ projectsite/studentorg/management/commands/create_initial_data.py
@@ -6,9 +6,51 @@ class Command(BaseCommand):
     help = 'Create initial data for the application'
 
     def handle(self, *args, **kwargs):
+        self.create_colleges()
+        self.create_programs()
         self.create_organization(10)
         self.create_students(50)
         self.create_membership(10)
+
+    def create_colleges(self):
+        colleges = [
+            'College of Engineering and Technology',
+            'College of Sciences',
+            'College of Humanities and Social Sciences',
+            'College of Business and Accountancy',
+            'College of Education',
+            'College of Nursing and Health Sciences',
+        ]
+        for college_name in colleges:
+            College.objects.get_or_create(college_name=college_name)
+
+        self.stdout.write(self.style.SUCCESS(
+            'Initial data for colleges created successfully.'
+        ))
+
+    def create_programs(self):
+        programs = [
+            ('Bachelor of Science in Computer Science', 'College of Engineering and Technology'),
+            ('Bachelor of Science in Information Technology', 'College of Engineering and Technology'),
+            ('Bachelor of Science in Civil Engineering', 'College of Engineering and Technology'),
+            ('Bachelor of Science in Biology', 'College of Sciences'),
+            ('Bachelor of Science in Mathematics', 'College of Sciences'),
+            ('Bachelor of Arts in Communication', 'College of Humanities and Social Sciences'),
+            ('Bachelor of Science in Psychology', 'College of Humanities and Social Sciences'),
+            ('Bachelor of Science in Accountancy', 'College of Business and Accountancy'),
+            ('Bachelor of Science in Business Administration', 'College of Business and Accountancy'),
+            ('Bachelor of Secondary Education', 'College of Education'),
+            ('Bachelor of Elementary Education', 'College of Education'),
+            ('Bachelor of Science in Nursing', 'College of Nursing and Health Sciences'),
+        ]
+        for prog_name, college_name in programs:
+            college = College.objects.filter(college_name=college_name).first()
+            if college:
+                Program.objects.get_or_create(prog_name=prog_name, college=college)
+
+        self.stdout.write(self.style.SUCCESS(
+            'Initial data for programs created successfully.'
+        ))
 
     def create_organization(self, count):
         fake = Faker()
